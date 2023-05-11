a5d05bad69687cf1cc8a433aee28e3c056c51951
Dhaval150 <d.r.chavda1509@gmail.com>
2023-05-11 17:45:07 +0530

    Bootstrap 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BadgeComponent } from './badge/badge.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { CardsComponent } from './cards/cards.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'badge', component:BadgeComponent},
+  {path: 'buttons', component:ButtonsComponent},
+  {path: 'cards', component: CardsComponent},
+  {},
+  {},
+  {},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BadgeComponent } from './badge/badge.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BadgeComponent,
+    ButtonsComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
